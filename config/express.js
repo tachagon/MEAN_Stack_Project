1ff9737bc9064b2f487fa88aea43e92e42731f6c
Tatchagon <o_k_t@hotmail.com>
2016-08-16 14:04:19 +0700

    start

---
+++ config/express.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var morgan = require('morgan');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var sass = require('node-sass-middleware');
+
+module.exports = function () {
+  var app = express();
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else {
+    app.use(compression);
+  }
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+
+  app.set('views', './app/views');
+  app.set('view engine', 'jade');
+
+  require('../app/routes/index.routes')(app);
+  require('../app/routes/user.routes')(app);
+
+  app.use(sass({
+    src: './sass',
+    dest: './public/css',
+    outputStyle: 'compressed',
+    prefix: '/css',
+    debug: true
+  }));
+
+  app.use(express.static('./public'));
+
+  return app;
+}
