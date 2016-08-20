c04a77684bf57331ff4c2fdbbf4bec577a6b58c7
Tatchagon <o_k_t@hotmail.com>
2016-08-20 12:37:20 +0700

    [5-4] CRUD: Read document ด้วย find และ findOne

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -52,3 +52,30 @@ exports.create = function (req, res, next) {
         }
     });
 };
+
+exports.list = function (req, res, next) {
+    User.find({}, function (err, users) {
+        if (err) {
+            return next(err);
+        }else {
+            res.json(users);
+        }
+    });
+};
+
+exports.read = function (req, res) {
+    res.json(req.user);
+};
+
+exports.userByUsername = function (req, res, next, username) {
+    User.findOne({
+        username: username
+    }, function (err, user) {
+        if (err) {
+            return next(err);
+        } else {
+            req.user = user;
+            next();
+        }
+    });
+};
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,5 +2,11 @@ module.exports = function (app) {
   var user = require('../controllers/user.controller');
   app.post('/login', user.login);
   app.post('/logout', user.logout);
-  app.route('/user').post(user.create);
+  app.route('/user')
+    .post(user.create)
+    .get(user.list);
+  app.route('/user/:username')
+    .get(user.read);
+
+  app.param('username', user.userByUsername);
 };
